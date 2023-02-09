57a7f15f1f244587d1eeb6cfc33322dda5b073e6
NilsonKuroki <45184768+NilsonKuroki@users.noreply.github.com>
2023-02-08 21:22:00 -0300

    add teams created

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react'
+import { 
+  TextField, Select, FormControl, MenuItem, InputLabel, Button
+ } from '@mui/material';
 import {
   ContainerTeam
 } from './styles'
@@ -6,34 +9,55 @@ import axios from 'axios';
 
 const Team = ()=>{
   const [teams, setTeams] = useState([])
-  const [name, setName] = useState([])
+  const [teamIdSelect, setTeamIdSelect] = useState("")
+  const [name, setName] = useState("")
   useEffect(()=>{
-    if(!teams){
+    if(!teams[0]){
       getTeams()
     }
   })
+
   const getTeams = async ()=>{
     const result = await axios.get("http://localhost:3333/team/")
     setTeams(result.data)
   }
   const handleAddTeam = async(name)=>{
-    await axios.post("http://localhost:3333/team/", {
-      name
-    })
+    await axios.post("http://localhost:3333/team/", {name})
     await getTeams()
+    setName("")
   }
+
   return(
     <ContainerTeam>
       <div>
-      {
-        teams.map((team,index)=>{
-          return(
-            <div key={index}>{team.name}</div>
-          )
-        })
-      }</div>
-      <input value={name} onChange={(e)=>{setName(e.target.value)}}/>
-      <button onClick={()=>{handleAddTeam(name)}}>Adicionar time</button>
+        <FormControl fullWidth>
+          <InputLabel id="input-select">Times</InputLabel>
+          <Select
+            labelId="input-select"
+            id="team-select"
+            value={teamIdSelect}
+            label="Times"
+            onChange={(event)=>{setTeamIdSelect(event.target.value)}}
+          >
+            {teams.map((team, index)=>{
+              return(
+                <MenuItem 
+                  key={index} 
+                  value={team.id} 
+                >
+                  {team.name}
+                </MenuItem>
+            )})}
+          </Select>
+        </FormControl>
+      </div>
+      <TextField 
+        label="adicione um time" 
+        variant="outlined" 
+        value={name}
+        onChange={(e)=>{setName(e.target.value)}}
+      />
+      <Button variant="outlined" onClick={()=>{handleAddTeam(name)}}>Adicionar time</Button>
     </ContainerTeam>
   )
 }
